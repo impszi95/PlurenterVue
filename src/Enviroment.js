009045e4d5a1be510774c783ca4dc9a83a8d8634
Varga Bence <impszi95@gmail.com>
2021-01-30 20:05:14 +0100

    prod ip

--- src/Enviroment.js
+++ src/Enviroment.js
@@ -1,10 +1,10 @@
-//const url = window.location.origin;
+const url = window.location.origin;
 
 const IPs = {
- // API_URL:url+"/api/",
- // WS_URL:url+"/ws/"
-    API_URL:"http://localhost:8081/api/",
-    WS_URL:"http://localhost:8082/ws/"
+  API_URL:url+"/api/",
+  WS_URL:url+"/ws/"
+   // API_URL:"http://localhost:8081/api/",
+    //WS_URL:"http://localhost:8082/ws/"
 }
 
 export default IPs

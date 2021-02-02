f4de23c2d8cdd4068d659d821d9c11ace515fc59
Varga Bence <impszi95@gmail.com>
2021-02-02 17:11:09 +0100

    Info scroll down

--- src/components/ActualMeet.vue
+++ src/components/ActualMeet.vue
@@ -2,48 +2,62 @@
   <div>
     <div class="haveMeet" v-if="!isEmpty">
       <div class="user">
-      <div class="username">
-        {{ user.username }}
+        <div class="username">
+          {{ user.username }}
+        </div>
+        <b-button
+          rounded
+          class="btn_info"
+          type="is-info"
+          @click="Info()"          
+          inverted
+        >
+          <b-icon
+            class="file-icon"
+            size="is-medium"
+            icon="information"
+          ></b-icon>
+        </b-button>
+        <b-carousel
+          v-if="actualPhoto != null"
+          :indicator-inside="true"
+          :autoplay="false"
+          icon-size="is-medium"
+        >
+          <b-carousel-item v-for="photo in user.photos" :key="photo.id">
+            <div class="image_container">
+              <img
+                class="image"
+                :src="'data:image/jpg;base64,' + photo.image.data"
+              />
+            </div>
+          </b-carousel-item>
+        </b-carousel>
+        <div v-else class="image_container">
+          <img class="image" src="../assets/default.png" />
+        </div>
       </div>
-      <b-carousel
-        v-if="actualPhoto != null"
-        :indicator-inside="true"
-        :autoplay="false"
-      >
-        <b-carousel-item v-for="photo in user.photos" :key="photo.id">
-          <div class="image_container">
-            <img
-              class="image"
-              :src="'data:image/jpg;base64,' + photo.image.data"
-            />
-          </div>
-        </b-carousel-item>
-      </b-carousel>
-      <div v-else  class="image_container">
-      <img class="image" src="../assets/default.png" />
+      <div class="buttons">
+        <b-button
+          rounded
+          class="btn"
+          @click="Dislike()"
+          type="is-info"
+          icon="upload"
+        >
+          <b-icon class="file-icon" size="is-medium" icon="close"></b-icon>
+        </b-button>
+
+        <b-button rounded class="btn" @click="Like()" type="is-danger">
+          <b-icon class="file-icon" size="is-medium" icon="heart"></b-icon>
+        </b-button>
       </div>
-    </div>
-    <div class="buttons">
-      <b-button
-        rounded
-        class="btn"
-        @click="Dislike()"
-        type="is-info"
-        icon="upload"
-      >
-        <b-icon class="file-icon" size="is-medium" icon="close"></b-icon>
-      </b-button>    
-      <b-button rounded class="btn" @click="Like()" type="is-danger">
-        <b-icon class="file-icon" size="is-medium" icon="heart"></b-icon>
-      </b-button> 
-      
-    </div>
-    <div class="details_label">Details</div>
-    <DatasTenant v-if="user.tenant" :user="user" />
-    <DatasLandlord v-else :user="user"/>
+      <div id="datas" class="details_label">Details</div>
+      <DatasTenant v-if="user.tenant" :user="user" />
+      <DatasLandlord v-else :user="user" />
     </div>
     <div v-if="isEmpty" class="loading">
-      <Loading/>
+      <Loading />
     </div>
   </div>
 </template>
@@ -53,34 +67,34 @@ import PlurenterService from "@/Services/PlurenterService";
 import Loading from "./Loading.vue";
 import DatasLandlord from "./Landlord/DatasLandlord";
 import DatasTenant from "./Tenant/DatasTenant";
+import $ from "jquery";
 
 export default {
   data() {
     return {
       user: null,
-      isEmpty:true,
+      isEmpty: true,
       actualPhoto: null,
     };
   },
   components: {
     Loading,
     DatasLandlord,
-    DatasTenant
-    },
+    DatasTenant,
+  },
   methods: {
     async loadActualMeet() {
       let user = await PlurenterService.actualMeet();
       if (user.id == null && user.username == null && user.photos == null) {
         this.isEmpty = true;
-      }
-      else{
+      } else {
         this.isEmpty = false;
       }
 
       if (user.photos != null) {
         this.actualPhoto = user.photos[0];
       } else {
-        this.actualPhoto = null
+        this.actualPhoto = null;
       }
       this.user = user;
     },
@@ -91,6 +105,14 @@ export default {
     async Dislike() {
       await PlurenterService.dislike();
       await this.loadActualMeet();
+    },
+    Info() {
+      $([document.documentElement, document.body]).animate(
+        {
+          scrollTop: $("#datas").offset().top,
+        },
+        800
+      );
     },
   },
   async created() {
@@ -122,15 +144,16 @@ export default {
   font: bold;
   height: 50px;
   width: 100%;
-  border:transparent;
+  border: transparent;
   border-bottom: rgb(201, 200, 200);
   border-style: solid;
-  border-width: 1px; 
+  border-width: 1px;
 }
 .image_container {
   width: 500px;
   height: 500px;
   position: relative;
+  background-color: #cecece;
 }
 @media only screen and (max-width: 550px) {
   [class*="image_container"] {
@@ -164,7 +187,19 @@ export default {
   height: 70px;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 5px 15px 0 rgba(0, 0, 0, 0.19);
 }
-.details_label{
+.btn_info {
+  width: 50px;
+  height: 50px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 5px 15px 0 rgba(0, 0, 0, 0.14);
+  margin: 0;
+  padding: 0;
+  position: absolute;
+  bottom: 8px;
+  right: 8px;
+  font-size: 18px;
+  z-index: 999;
+}
+.details_label {
   font-size: 1.4rem;
   margin-bottom: -20px;
 }
